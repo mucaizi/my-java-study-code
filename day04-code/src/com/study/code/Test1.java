8623591ea0cbac7d7985987207bece0702db8ab7
mucaizi <2827625657@qq.com>
2026-06-02 22:55:25 +0800

    day4

---
+++ day04-code/src/com/study/code/Test1.java
@@ -0,0 +1,12 @@
+package com.study.code;
+
+public class Test1 {
+    static void main() {
+        int money = 1000000;
+        if(money > 100){
+            System.out.println("吃网红餐厅");
+        }else {
+            System.out.println("吃土");
+        }
+    }
+}
---
+++ day04-code/src/com/study/code/Test4.java
@@ -0,0 +1,9 @@
+package com.study.code;
+
+public class Test4 {
+    static void main() {
+        for(int i = 0; i < 5;i++){
+            System.out.println("断线重连");
+        }
+    }
+}
